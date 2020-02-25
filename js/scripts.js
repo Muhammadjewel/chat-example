bf25b243e617dc2eb113028875a5ca23bfcb4594
muhammadjewel <msuratov@gmail.com>
2020-02-25 15:50:44 +0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// var menyu = document.querySelector(".menyu-sticky");
+
+// window.addEventListener("scroll", function(evt) {
+// 	if (menyu.scrollTop > 60) {
+// 		menyu.classList.add("menyu-fixedli");
+// 	} else {
+// 		menyu.classList.remove("menyu-fixedli");
+// 	}
+// });
